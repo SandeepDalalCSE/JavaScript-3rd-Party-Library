eef2e9d1d392c3929a7b22d3d361eaa74dd1df95
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 21:57:30 +0530

    using _.difference() method of lodash.min.js, finding the difference between two arrays.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 // Question. Suppose we have two arrays, say customers[] and activeCustomers[]. If need to find the inactive customers in inactiveCustomers[]. then using 3rd party library we can easily find the difference of these two arrays to find the inactiveCustomers[].
 const customers = ["Sandeep", "Ajay", "Ankit", "Deepanshu"];
 const activeCustomers = ["Ajay", "Deepanshu"];
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);
