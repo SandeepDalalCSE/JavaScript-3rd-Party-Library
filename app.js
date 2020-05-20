74aaa55958eef49001c1da20249e3a8014cca036
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-20 21:47:24 +0530

    adding important link to index.html and question in app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+// Question. Suppose we have two arrays, say customers[] and activeCustomers[]. If need to find the inactive customers in inactiveCustomers[]. then using 3rd party library we can easily find the difference of these two arrays to find the inactiveCustomers[].
+const customers = ["Sandeep", "Ajay", "Ankit", "Deepanshu"];
+const activeCustomers = ["Ajay", "Deepanshu"];
